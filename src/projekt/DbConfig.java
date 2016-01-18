a96f623741e8c22e7ff70204cdbefaf51d41984c
Kacper Kołodziej <kacper@kolodziej.in>
2016-01-18 18:10:40 +0100

    plik config z hasłem do bazy

---
+++ src/projekt/DbConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt;
+
+/**
+ *
+ * @author kacper
+ */
+public class DbConfig {
+    public static final String server = "s1.kolodziej.it";
+    public static final String username = "hospital";
+    public static final String password = "c@2ea(*1FsE10cd91F7h";
+    public static final String dbname = "Hospital";
+}
+//FC0hZ3MsbMLF
